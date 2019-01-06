158bee40ec89ac1b819be202f8f8a5349af31e98
helloitsjoe <jboyle617@gmail.com>
2019-01-05 22:41:40 -0500

    fix instructions

--- src/__tests__/utils-spec.jest.js
+++ src/__tests__/utils-spec.jest.js
@@ -1,15 +1,9 @@
-// 0. Test with if/else
-// Note: Use require() instead of import
 // 1. Test for string validation
 // 2. Test for number invalidation
 // 3. test.each for edge cases:
-// String valid
-// Number invalid
 // Single letter valid
 // Single number invalid
 // letters with spaces valid
 // Mixed letters/numbers invalid
 // Symbols invalid
 // Only spaces invalid
-
-import { validateInput } from '../utils';
