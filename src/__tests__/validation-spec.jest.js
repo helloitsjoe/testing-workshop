ad15c014f335f862b31c5e4632d507ee722e5aab
Joe Boyle <joboyle@wayfair.com>
2019-01-07 17:32:26 -0500

    tweaks

--- src/__tests__/validation-spec.jest.js
+++ src/__tests__/validation-spec.jest.js
@@ -1,9 +1,10 @@
 // 1. Test for string validation
 // 2. Test for number invalidation
+
+// later:
 // 3. test.each for edge cases:
 // Single letter valid
 // Single number invalid
 // letters with spaces valid
 // empty string valid
 // Mixed letters/numbers invalid
-// Symbols invalid
