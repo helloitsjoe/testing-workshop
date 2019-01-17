91c8563d242498df48459c6dc302ebe2a8274ad1
helloitsjoe <jboyle617@gmail.com>
2019-01-16 20:28:00 -0500

    minor update

--- src/user-input-container.js
+++ src/user-input-container.js
@@ -1,5 +1,5 @@
 // 1. h1 that says "Welcome, <user>!"
-// 2. UserInput component with props: valid, inputValue, onChange, onSubmit
+// 2. UserInput component with props: valid, onChange, onSubmit
 // 3. state = {name, inputValue, valid}
 // handleChange
 // handleSubmit
