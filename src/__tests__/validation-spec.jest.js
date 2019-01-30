73d6ac4fd04bf3091f8067c975df84192ab1ce09
Joe Boyle <joboyle@wayfair.com>
2019-01-30 13:58:53 -0500

    remove code, keep imports

--- src/__tests__/validation-spec.jest.js
+++ src/__tests__/validation-spec.jest.js
@@ -1,13 +1,1 @@
 import { validate } from '../validation';
-
-it.each`
-  description                  | input    | output
-  ${'letters are valid'}       | ${'Joe'} | ${true}
-  ${'spaces are valid'}        | ${' '}   | ${true}
-  ${'single letter valid'}     | ${'a'}   | ${true}
-  ${'capital letter valid'}    | ${'A'}   | ${true}
-  ${'numbers are invalid'}     | ${'9'}   | ${false}
-  ${'letters/numbers invalid'} | ${'a9'}  | ${false}
-`('$description', ({ input, output }) => {
-  expect(validate(input)).toBe(output);
-});
--- src/validation.js
+++
@@ -1,1 +0,0 @@
-export const validate = name => /^[a-z ]*$/i.test(name);
