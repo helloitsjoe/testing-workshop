4984cd96d1a56a76fa2fb47e4a656c6845d23dfd
helloitsjoe <jboyle617@gmail.com>
2019-07-14 23:13:06 -0400

    setup files with react and empty components

--- src/user-input-container.js
+++ src/user-input-container.js
@@ -1,3 +1,14 @@
+import React, { Component } from 'react';
 import UserInput from './user-input';
-import BannerWrapper from './banner-wrapper';
+import Banner from './banner';
 import { validate } from './validation';
+
+class UserInputContainer extends Component {
+  state = {};
+
+  render() {
+    return null;
+  }
+}
+
+export default UserInputContainer;
